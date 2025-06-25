4b2c7e36a46abcf61e04662faee2915ee8a5fa07
Olu Sampson <remsams29@yahoo.com>
2025-06-25 23:16:48 +0100

    Switch to standard 'out' directory for Netlify compatibility

--- Downloads/GAMESOFT/Playhost-NextJs/next.config.js
+++ Downloads/GAMESOFT/Playhost-NextJs/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
   output: 'export',
-  distDir: '_static',
+  distDir: 'out',
   webpack: (config, { webpack }) => {
     config.plugins.push(
       new webpack.ProvidePlugin({

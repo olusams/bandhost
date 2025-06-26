055a87457bce6de3f4fe076ec3b87770ebbde60f
Olu Sampson <60044354+olusams@users.noreply.github.com>
2025-06-26 08:15:50 +0100

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: '',
+  basePath: '',
+  webpack: (config, { webpack }) => {
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        "window.jQuery": "jquery",
+      })
+    );
+    return config;
+  },
+}
+
+module.exports = nextConfig
